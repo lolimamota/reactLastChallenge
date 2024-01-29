a206c9110c6e24485634b3bd5d02395a47a8574d
Lorrayne <lorraynelimamota@gmail.com>
2024-01-29 12:25:46 -0300

    add: ul on Footer

--- fantastika_react/src/Componets/Footer/Footer.jsx
+++ fantastika_react/src/Componets/Footer/Footer.jsx
@@ -7,9 +7,29 @@ function Footer(){
         <>
         <img src={Americanas} alt="Logo da Loja Americanas"/>
         <img src={Havaianas} alt="Logo da Loja Havaianas"/>
-        <ul>Atendimento</ul>
-        <ul>Sobre</ul>
-        <ul>Unidades</ul>
+        <div>
+            <h2>Atendimento</h2>
+            <ul>
+                <li>Chat virtual</li>
+                <li>Whatsapp</li>
+                <li>Telefone</li>
+                <li>Email</li>
+            </ul>
+            <h2>Sobre</h2>
+            <ul>
+                <li>Nosso trabalho</li>
+                <li>Infraestrutura</li>
+                <li>Nossas Unidades</li>
+                <li>Especialistas</li>
+            </ul>
+            <h2>Unidades</h2>
+            <ul>
+            <li>Clinica RJ</li>
+            <li>Clinica SP</li>
+            <li>Clinica ES</li>
+            <li>Clinica SC</li>
+            </ul>
+        </div>
         </>
     )
 }
