1686ffbd30ef1c0413e1d16f7a607129a9f6cb1d
Lorrayne <lorraynelimamota@gmail.com>
2024-01-31 00:39:25 -0300

    aprendi styled-components

--- fantastika_react/src/App.jsx
+++ fantastika_react/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './Componets/Header/Header.jsx'
 import Main from './Componets/Main/Main.jsx'
 import Footer from './Componets/Footer/Footer.jsx'
+import { Component } from 'react'
 
 function App() {
   return (
--- fantastika_react/src/Componets/Footer/Footer.jsx
+++ fantastika_react/src/Componets/Footer/Footer.jsx
@@ -1,62 +1,75 @@
 import React from "react";
+import styled from 'styled-components';
 import Americanas from "../Footer/img/logo_ame.png";
-import Havaianas from "../Footer/img/logo_hava.png"
-import LogoInsta from "../Header/img/logo_insta.png"
-import LogoYt from "../Header/img/logo_yt.png"
-import LogoWpp from "../Header/img/logo_wpp.png"
-import LogoFanta from "./img/logo_fanta.png"
+import Havaianas from "../Footer/img/logo_hava.png";
+import LogoInsta from "../Header/img/logo_insta.png";
+import LogoYt from "../Header/img/logo_yt.png";
+import LogoWpp from "../Header/img/logo_wpp.png";
+import LogoFanta from "./img/logo_fanta.png";
+
+const Display = styled.ul
+`display: flex;
+justify-content: center;
+align-itens: center;
+list-style:none;`;
+
+const Details = styled.li
+`list-style:none;`;
+
+
 
 function Footer() {
     return (
         <>
-        <div>
+        <div >
             <img src={Americanas} alt="Logo da Loja Americanas" />
             <img src={Havaianas} alt="Logo da Loja Havaianas" />
         </div>
         <section>
             <img src={LogoFanta} alt="Logo da Fantastika, uma patinha e o nome" />
             <div>
-                <ul>
-                    <li>
+                <Display>
+                    <Details>
                         <a href="https://www.instagram.com/its_anephilim/" target="_blank">
                         <img src={LogoInsta} alt="Logo do Instagram" /></a>
-                    </li>
-                    <li>
+                    </Details>
+                    <Details>
                         <a href='https://www.youtube.com/' target="_blank">
                         <img src={LogoYt} alt="Logo do YouTube" /></a>
-                    </li>
-                    <li>
+                    </Details>
+                    <Details>
                         <a href="https://www.whatsapp.com/?lang=pt_BR" target="_blank">
                         <img src={LogoWpp} alt="Logo do WhatsApp" /></a>
-                    </li>
-                </ul>
+                    </Details>
+                </Display>
             </div>
-            <div>
+            <Display>
                 <h2>Atendimento</h2>
                 <ul>
-                    <li>Chat virtual</li>
-                    <li>Whatsapp</li>
-                    <li>Telefone</li>
-                    <li>Email</li>
+                    <Details>Chat virtual</Details>
+                    <Details>Whatsapp</Details>
+                    <Details>Telefone</Details>
+                    <Details>Email</Details>
                 </ul>
                 <h2>Sobre</h2>
                 <ul>
-                    <li>Nosso trabalho</li>
-                    <li>Infraestrutura</li>
-                    <li>Nossas Unidades</li>
-                    <li>Especialistas</li>
+                    <Details>Nosso trabalho</Details>
+                    <Details>Infraestrutura</Details>
+                    <Details>Nossas Unidades</Details>
+                    <Details>Especialistas</Details>
                 </ul>
                 <h2>Unidades</h2>
                 <ul>
-                    <li>Clinica RJ</li>
-                    <li>Clinica SP</li>
-                    <li>Clinica ES</li>
-                    <li>Clinica SC</li>
+                    <Details>Clinica RJ</Details>
+                    <Details>Clinica SP</Details>
+                    <Details>Clinica ES</Details>
+                    <Details>Clinica SC</Details>
                 </ul>
-            </div>
+            </Display>
         </section>
         </>
     )
 }
 
 export default Footer;
+
